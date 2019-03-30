a20c8cf27873ba7ca9604a92d7eb00736c3e8d55
Nivethan Sethupathi <nivethan.sethupathi@gmail.com>
2019-03-30 17:02:34 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# My Sorts
+These are my Sorts. 
---
+++ src/app/App.java
@@ -0,0 +1,14 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+
+        int[] nums = {10, 7, 86, 24, 43, 4, 14, 8, 47, 34};
+    }
+
+    public void InsertionSort()
+    {
+
+    }
+}
