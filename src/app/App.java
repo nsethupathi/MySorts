032c30924b3000a30f469d90bdb385bed4bcbd31
Nivethan Sethupathi <nivethan.sethupathi@gmail.com>
2019-03-30 17:08:10 -0500

    added two more ints to nums

--- src/app/App.java
+++ src/app/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
 
-        int[] nums = {10, 7, 86, 24, 43, 4, 14, 8, 47, 34};
+        int[] nums = {10, 7, 86, 24, 43, 4, 14, 8, 47, 34, 54, 53};
     }
 
     public void InsertionSort()
